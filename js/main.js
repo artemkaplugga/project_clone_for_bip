8676fe9f13d1b39611ce4bcf70ebbfa712fa7bb0
Максим Афанасьев <Afanasiev2023@spobip-bel.ru>
2025-02-24 08:46:31 +0300

    added fonts

---
+++ js/main.js
@@ -0,0 +1,12 @@
+new Swiper('.destination__swiper', {
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    spaceBetween: 25,
+});
+
+new Swiper('.categories__swiper', {
+    slidesPerView: 6,
+    slidesPerGroup: 1,
+    spaceBetween: 30,
+    loop: true
+});
