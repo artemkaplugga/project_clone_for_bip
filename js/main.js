1d6f108c38adb349f89f9f126a3dd3679fd14f31
Максим Афанасьев <Afanasiev2023@spobip-bel.ru>
2025-02-28 10:07:17 +0300

    added navigations and fixed some problems

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 new Swiper('.destination__swiper', {
     slidesPerView: 3,
     slidesPerGroup: 1,
-    spaceBetween: 25,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: "#nextDestinationBtn",
+        prevEl: "#prevDestinationBtn",
+    },
 });
 
 new Swiper('.categories__swiper', {
     slidesPerView: 6,
     slidesPerGroup: 1,
     spaceBetween: 30,
-    loop: true
+    loop: true,
+    navigation: {
+        nextEl: "#nextBtn",
+        prevEl: "#prevBtn",
+    },
+});
+
+const emailInput = document.querySelector('.email__address');
+const submitButton = document.querySelector('#tg_btn');
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    if (emailInput.value.trim() !== '') {
+        alert(`Введенный email: ${emailInput.value}`);
+    } else {
+        alert('Поле email пустое');
+    }
 });
